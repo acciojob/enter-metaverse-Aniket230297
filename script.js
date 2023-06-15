012c7426eedf098d8174c2127764c917c5cf2cbe
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-06-15 14:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //your JS code here. If required.
-function func(){
-	let p=document.getElementById("status");
-	p.innerHTML="Entered Metaverse";
-}
+const statusElement = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+let head = document.getElementById('status');
+
+ enterButton.addEventListener("click", function() {
+   const h1Element = document.createElement("h1");
+  h1Element.id ="status";
+    h1Element.textContent = "Entered Metaverse";
+	 head.appendChild(h1Element);
+     statusElement.parentNode.replaceChild(h1Element, statusElement);
+});
